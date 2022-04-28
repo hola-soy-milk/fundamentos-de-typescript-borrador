ee65cf72aade314c7e086255848d9db38aef4d20
Ramón Huidobro <hola@ramonh.dev>
2022-04-28 11:29:44 +0200

    Remove logo

--- leccion-03-primera-interfaz/src/App.tsx
+++ leccion-03-primera-interfaz/src/App.tsx
@@ -1,5 +1,4 @@
 import React, {useState, useEffect} from 'react';
-import logo from './logo.svg';
 import './App.css';
 import PostList from './components/PostList';
 import PostForm from './components/PostForm';
